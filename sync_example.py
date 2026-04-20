db0231b1643b370a95259229695716dbc2faef17
Panos Kounoudis <panos.phil@gmail.com>
2026-04-20 20:23:24 +0300

    snippets to understand async in python

---
+++ sync_example.py
@@ -0,0 +1,14 @@
+import time
+
+def task1():
+    print("Task 1 started")
+    time.sleep(2)
+    print("Task 1 finished")
+
+def task2():
+    print("Task 2 started")
+    time.sleep(1)
+    print("Task 2 finished")
+
+task1()
+task2()
