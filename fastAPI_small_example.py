b70983e491d0c0d60f9c40c7b008225167d93cc4
Panos Kounoudis <panos.phil@gmail.com>
2026-04-28 14:15:21 +0300

    a comment

--- fastAPI_small_example.py
+++ fastAPI_small_example.py
@@ -1,7 +1,9 @@
+# imports
 from fastapi import FastAPI
 
 app = FastAPI()
 
+# define endpointls
 @app.get("/")
 async def root():
     return {"message": "Hello World, You're rocking! This is your first FastAPI app!"}
